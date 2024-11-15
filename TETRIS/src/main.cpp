d24a508a31e1600daaa052a03c120f88eb44fd44
thomas-serdjebi <thomas.serdjebi@laplateforme.io>
2024-11-15 16:15:51 +0100

    Fenêtre fonctionnelle

--- TETRIS/src/main.cpp
+++ TETRIS/src/main.cpp
@@ -3,12 +3,14 @@
 
 int main() 
 {
+    Color darkBlue = {44, 44, 127, 255};
     InitWindow(300,600, "TETRIS");
     SetTargetFPS(60);
 
     while(WindowShouldClose() == false) 
     {
         BeginDrawing();
+        ClearBackground(darkBlue);
         EndDrawing();
     }
 
